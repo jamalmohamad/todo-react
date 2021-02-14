5b6a76bf18e010ae14decc35a88ef21a99584192
jamalmohamad <mohammed.jamalhadi@gmail.com>
2021-02-14 18:26:17 +0100

    feat: create components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,32 @@ import React, { Component  } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import MyFirstComponent from './MyComponent';
+
 class App extends Component {
   render() {
     return(
       <div className="App">
         My Hello World
+
+        <FirstComponent></FirstComponent>
+        <MyFirstComponent></MyFirstComponent>
       </div>
     );
   }
 }
+
+
+
+class FirstComponent extends Component {
+  render(){
+    return (
+      <div className="firstComponent">
+        My First Component
+      </div>
+    )
+  }
+}
+
+
 export default App;
---
+++ src/MyComponent.js
@@ -0,0 +1,16 @@
+import React, { Component  } from 'react'
+
+class MyFirstComponent extends Component {
+    render() {
+        return (
+            <div className="myFirstComponent">
+                Hello, I am from myFirstComponent
+            </div>
+        )
+    }
+
+}
+
+
+
+export default MyFirstComponent; 

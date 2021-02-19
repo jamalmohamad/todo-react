546108bfe8674238b607acd65da86fc66f91257d
jamalmohamad <mohammed.jamalhadi@gmail.com>
2021-02-19 22:47:51 +0100

    feat: refactor HeaderComponent and fix the dynamic menu link refresh

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -3,6 +3,7 @@ import React, { Component  } from 'react';
 import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 import AuthenticationService from './AuthenticationService';
 import AuthenticatedRoute from './AuthenticatedRoute.jsx';
+import HeaderComponent from './HeaderComponent.jsx';
 
 // Switch ensures only one component is active 
 class TodoApp extends Component {
@@ -27,45 +28,6 @@ class TodoApp extends Component {
     }
 }
 
-
-class HeaderComponent extends Component {    
-    render() {
-        const isUserLoggedIn = AuthenticationService.isUserLoggedIn();
-        console.log(isUserLoggedIn);
-
-
-        return (
-            <header>
-                <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-                    <div><a href="localhost:3000" className="navbar-brand">in28Minutes</a></div>
-                    <ul className="navbar-nav">
-                        {
-                            isUserLoggedIn &&
-                            <li><Link className="nav-link" to="/welcome/in28minutes">Home</Link></li> 
-                        }
-                        {
-                            isUserLoggedIn &&
-                            <li><Link className="nav-link" to="/todos">Todos</Link></li>
-                        }
-                    </ul>
-
-                    <ul  className="navbar-nav navbar-collapse justify-content-end">
-                        {
-                            !isUserLoggedIn &&
-                            <li><Link className="nav-link" to="/login">Login</Link></li>
-                         }
-                         {
-                             isUserLoggedIn &&
-                            <li><Link className="nav-link" to="/logout" onClick={AuthenticationService.logout}>Logout</Link></li>
-                         }
-                    </ul>
-                </nav>
-
-            </header>
-            
-        )
-    }
-}
 
 
 class FooterComponent extends Component {
